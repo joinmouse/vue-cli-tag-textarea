4b57491f2d3723f66ff9b0df0cc51a79362e3de6
joinmouse <908380434@qq.com>
2019-09-30 17:31:03 +0800

    更新到0.0.2

--- packages/textarea/main.vue
+++ packages/textarea/main.vue
@@ -1,5 +1,5 @@
 <template>
-    <textarea name="" id="" cols="30" rows="20"></textarea>
+    <textarea id="editor" cols="100" rows="20" placeholder="请开始你的创作..."></textarea>
 </template>
 
 <script>
@@ -8,5 +8,12 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+textarea {
+    margin: 100px auto 0;
+    padding: 10px;
+    display: block;
+    font-size: 18px;
+    font-family: Arial, Helvetica, sans-serif
+}
 </style> 
